e1d5b1ca35f7165efbab1485371176fc8f1c233a
Thatcher Peskens <thatcher@dotcloud.com>
2013-08-13 18:57:32 -0700

    Renamed the app-directory to prevent possible naming conflicts and extended teh MANIFEST.in to include staticfiles, templates and find_packages()

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='DockerTutorial',
     version='0.1.0',
     author=u'Thatcher Peskens',
     author_email='thatcher@dotcloud.com',
-    packages=['tutorial'],
+    packages=find_packages(),
     url='https://github.com/dhrp/docker-tutorial/',
     license='To be determined',
     description='An interactive learning environment to get familiar with the Docker cli',
     long_description=open('README.txt').read(),
+    include_package_data=True,
 )
