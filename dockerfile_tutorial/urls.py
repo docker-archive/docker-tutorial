21c7e266979d784b3c7be289f9cf1698a19e2c5c
Thatcher Peskens <thatcher@dotcloud.com>
2013-08-22 13:19:54 -0700

    added requires_csrf_token decorator to views

--- dockerfile_tutorial/urls.py
+++ dockerfile_tutorial/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView
+from django.views.decorators.csrf import requires_csrf_token
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -13,8 +14,8 @@ urlpatterns = patterns('',
 
     url(r'^$', RedirectView.as_view(url=reverse_lazy('dockerfile'))),
 
-    url(r'^dockerfile/$', TemplateView.as_view(template_name='dockerfile/introduction.html'), name='dockerfile'),
-    url(r'^dockerfile/level1/$', TemplateView.as_view(template_name='dockerfile/level1.html'), name='dockerfile_level1'),
+    url(r'^dockerfile/$', requires_csrf_token(TemplateView.as_view(template_name='dockerfile/introduction.html')), name='dockerfile'),
+    url(r'^dockerfile/level1/$', requires_csrf_token(TemplateView.as_view(template_name='dockerfile/level1.html')), name='dockerfile_level1'),
 
 )
 
