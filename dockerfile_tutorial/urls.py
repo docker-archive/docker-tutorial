d54b94af4bff07c3a97d8ece5c5a8f4665b4096d
Thatcher Peskens <thatcher@dotcloud.com>
2013-08-22 13:39:33 -0700

    replaced requires_csrf_token with ensure_csrf_cookie

--- dockerfile_tutorial/urls.py
+++ dockerfile_tutorial/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import RedirectView
-from django.views.decorators.csrf import requires_csrf_token
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -14,8 +14,8 @@ urlpatterns = patterns('',
 
     url(r'^$', RedirectView.as_view(url=reverse_lazy('dockerfile'))),
 
-    url(r'^dockerfile/$', requires_csrf_token(TemplateView.as_view(template_name='dockerfile/introduction.html')), name='dockerfile'),
-    url(r'^dockerfile/level1/$', requires_csrf_token(TemplateView.as_view(template_name='dockerfile/level1.html')), name='dockerfile_level1'),
+    url(r'^dockerfile/$', ensure_csrf_cookie(TemplateView.as_view(template_name='dockerfile/introduction.html')), name='dockerfile'),
+    url(r'^dockerfile/level1/$', ensure_csrf_cookie(TemplateView.as_view(template_name='dockerfile/level1.html')), name='dockerfile_level1'),
 
 )
 
